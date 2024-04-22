b5791b08a1165d3712936d1b9b221870944b029d
Midra <me@midra.me>
2024-04-22 21:59:07 +0900

    バージョン表記追加

--- src/popup/SettingDrawer.tsx
+++ src/popup/SettingDrawer.tsx
@@ -1,11 +1,16 @@
-import { Collapse } from 'antd'
+import { Flex, Collapse, Typography } from 'antd'
 import { SettingOutlined } from '@ant-design/icons'
+import { webext } from '@/webext'
 import { useStorage } from '@/utils/storage'
 import { Drawer } from './components/Drawer'
 import { SettingAccount } from './setting/SettingAccount'
 import { SettingRoom } from './setting/SettingRoom'
 import { SettingVods } from './setting/SettingVods'
 import { SettingOthers } from './setting/SettingOthers'
+
+const { Text, Link } = Typography
+
+const manifest = webext.runtime.getManifest()
 
 export const SettingDrawer: React.FC<{
   open: boolean
@@ -22,40 +27,51 @@ export const SettingDrawer: React.FC<{
       setOpen={setOpen}
     >
       {open && (
-        <Collapse
-          size="small"
-          defaultActiveKey={['account']}
-          items={[
-            {
-              key: 'account',
-              label: 'アカウント',
-              children: <SettingAccount />,
-            },
-            myRoom && {
-              key: 'room',
-              label: '部屋',
-              children: <SettingRoom />,
-            },
-            {
-              key: 'vods',
-              label: '動画配信サービス',
-              children: <SettingVods />,
-            },
-            {
-              key: 'others',
-              label: 'その他',
-              children: <SettingOthers />,
-            },
-          ].flatMap((item) =>
-            item
-              ? {
-                  ...item,
-                  style: { borderRadius: 0 },
-                }
-              : []
-          )}
-          style={{ borderRadius: 0 }}
-        />
+        <Flex vertical>
+          <Collapse
+            size="small"
+            defaultActiveKey={['account']}
+            items={[
+              {
+                key: 'account',
+                label: 'アカウント',
+                children: <SettingAccount />,
+              },
+              myRoom && {
+                key: 'room',
+                label: '部屋',
+                children: <SettingRoom />,
+              },
+              {
+                key: 'vods',
+                label: '動画配信サービス',
+                children: <SettingVods />,
+              },
+              {
+                key: 'others',
+                label: 'その他',
+                children: <SettingOthers />,
+              },
+            ].flatMap((item) =>
+              item
+                ? {
+                    ...item,
+                    style: { borderRadius: 0 },
+                  }
+                : []
+            )}
+            style={{ borderRadius: 0 }}
+          />
+
+          <Link
+            type="secondary"
+            href={manifest.homepage_url}
+            target="_blank"
+            style={{ margin: '1em auto' }}
+          >
+            {manifest.name} v{manifest.version}
+          </Link>
+        </Flex>
       )}
     </Drawer>
   )
