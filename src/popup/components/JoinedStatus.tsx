015171cf212f38d3999d40950d66031c8594b15b
Midra <me@midra.me>
2024-04-26 18:57:24 +0900

    参加者一覧のドロワーを削除

--- src/popup/components/JoinedStatus.tsx
+++ src/popup/components/JoinedStatus.tsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import { Flex, Space, List, Button, Typography } from 'antd'
+import { Flex, Space, Typography } from 'antd'
 import {
   LoadingOutlined,
   TeamOutlined,
@@ -9,14 +8,10 @@ import {
 } from '@ant-design/icons'
 import { formatDuration } from '@/utils/formatDuration'
 import { useStorage } from '@/utils/storage'
-import { Drawer } from '@/popup/components/Drawer'
-import { UserItem } from '@/popup/components/UserItem'
 
 const { Paragraph, Text } = Typography
 
 export const JoinedStatus: React.FC<{ isHost?: boolean }> = ({ isHost }) => {
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
-
   const [room] = useStorage('joined_room')
   const [members] = useStorage('joined_members')
   const [ping] = useStorage('joined_state_ping')
@@ -25,76 +20,44 @@ export const JoinedStatus: React.FC<{ isHost?: boolean }> = ({ isHost }) => {
 
   return (
     room && (
-      <>
-        <Flex vertical gap="small">
-          <Flex justify="space-between">
-            <Button
-              type="text"
-              size="small"
-              onClick={() => setIsDrawerOpen(true)}
-            >
-              <Space>
-                <TeamOutlined />
-                <Text>{members?.length ?? 0}</Text>
-              </Space>
-            </Button>
+      <Flex vertical gap="small">
+        <Flex justify="space-between">
+          <Space>
+            <TeamOutlined />
+            <Text>{members?.length ?? 0}</Text>
+          </Space>
 
-            <Space>
-              {playing ? <PlayCircleOutlined /> : <PauseCircleOutlined />}
-              <Text>{formatDuration(playingTime ?? 0)}</Text>
-            </Space>
+          <Space>
+            {playing ? <PlayCircleOutlined /> : <PauseCircleOutlined />}
+            <Text>{formatDuration(playingTime ?? 0)}</Text>
+          </Space>
 
-            <Space>
-              {ping ? (
-                <>
-                  <WifiOutlined
-                    style={{ color: ping < 300 ? '#52c41a' : '#faad14' }}
-                  />
-                  <Text style={{ color: ping < 300 ? '#52c41a' : '#faad14' }}>
-                    {ping} ms
-                  </Text>
-                </>
-              ) : (
-                <>
-                  <LoadingOutlined spin style={{ color: '#1677ff' }} />
-                  <Text type="secondary">接続中...</Text>
-                </>
-              )}
-            </Space>
-          </Flex>
-
-          {!isHost && (
-            <Paragraph ellipsis style={{ margin: 0 }}>
-              <Text type="secondary">ホスト: </Text>
-              <Text strong>{room.resident}</Text>
-            </Paragraph>
-          )}
+          <Space>
+            {ping ? (
+              <>
+                <WifiOutlined
+                  style={{ color: ping < 300 ? '#52c41a' : '#faad14' }}
+                />
+                <Text style={{ color: ping < 300 ? '#52c41a' : '#faad14' }}>
+                  {ping} ms
+                </Text>
+              </>
+            ) : (
+              <>
+                <LoadingOutlined spin style={{ color: '#1677ff' }} />
+                <Text type="secondary">接続中...</Text>
+              </>
+            )}
+          </Space>
         </Flex>
 
-        <Drawer
-          placement="left"
-          width={250}
-          title="参加者一覧"
-          icon={<TeamOutlined />}
-          open={isDrawerOpen}
-          setOpen={setIsDrawerOpen}
-        >
-          <List
-            itemLayout="horizontal"
-            size="small"
-            dataSource={members}
-            renderItem={(member) => (
-              <List.Item style={{ padding: '0.5em 0.75em' }}>
-                <UserItem
-                  avatar={member.avatar}
-                  name={member.name}
-                  id={member.id}
-                />
-              </List.Item>
-            )}
-          />
-        </Drawer>
-      </>
+        {!isHost && (
+          <Paragraph ellipsis style={{ margin: 0 }}>
+            <Text type="secondary">ホスト: </Text>
+            <Text strong>{room.resident}</Text>
+          </Paragraph>
+        )}
+      </Flex>
     )
   )
 }
